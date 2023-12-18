ac60129c10757f06eebb0afa48654e0afaa2ee03
Lorenzobattistela <lorenzobattistela@gmail.com>
2023-12-18 14:22:08 -0300

    generated Expr.java file

---
+++ jlox/Expr.java
@@ -0,0 +1,68 @@
+package lox;
+
+import java.util.List;
+
+abstract class Expr {
+  interface Visitor<R> {
+    R visitBinaryExpr(Binary expr);
+    R visitGroupingExpr(Grouping expr);
+    R visitLiteralExpr(Literal expr);
+    R visitUnaryExpr(Unary expr);
+  }
+  static class Binary extends Expr {
+    Binary extends Expr {
+      this.left = left;
+      this.operator = operator;
+      this.right = right;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitBinaryExpr(this);
+    }
+
+    final Expr left;
+    final Token operator;
+    final Expr right;
+  }
+  static class Grouping extends Expr {
+    Grouping extends Expr {
+      this.expression = expression;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitGroupingExpr(this);
+    }
+
+    final Expr expression;
+  }
+  static class Literal extends Expr {
+    Literal extends Expr {
+      this.value = value;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitLiteralExpr(this);
+    }
+
+    final Object value;
+  }
+  static class Unary extends Expr {
+    Unary extends Expr {
+      this.operator = operator;
+      this.right = right;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitUnaryExpr(this);
+    }
+
+    final Token operator;
+    final Expr right;
+  }
+
+    abstract <R> R accept(Visitor<R> visitor);
+}
