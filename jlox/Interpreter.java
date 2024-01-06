b7dd62678bec830d9c9c50ddd4916e665e9e3a1c
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-01-06 13:39:53 -0300

    adding support for functions

--- jlox/Interpreter.java
+++ jlox/Interpreter.java
@@ -1,10 +1,27 @@
 package jlox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
-  private Environment environment = new Environment();
+  final Environment globals = new Environment();
+  private Environment environment = globals;
+
+  Interpreter() {
+    globals.define("clock", new LoxCallable() {
+      @Override
+      public int arity() { return 0; }
+
+      @Override
+      public Object call(Interpreter interpreter, List<Object> arguments) {
+        return (double)System.currentTimeMillis() / 1000.0;
+      }
+
+      @Override
+      public String toString() { return "<native fn>"; }
+    })
+  }
 
   void interpret(List<Stmt> statements) {
     try {
@@ -96,6 +113,25 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
       case EQUAL_EQUAL: return isEqual(left, right);
     }
     return null;
+  }
+
+  @Override
+  public Object visitCallExpr(Expr.call expr) {
+    Object callee = evaluate(expr.callee);
+    List<Object> arguments = new ArrayList<>();
+    for(Expr argument : expr.arguments) {
+      arguments.add(evaluate(argument));
+    }
+
+    if(!(callee instanceof LoxCallable)) {
+      throw new RuntimeError(expr.paren, "Can only call function and classes.");
+    }
+
+    LoxCallable function = (LoxCallable) callee;
+    if(arguments.size() != function.arity()) {
+      throw new RuntimeError(expr.paren, "Expected " + function.arity() + " arguments but got " + arguments.size() + ".");
+    }
+    return function.call(this, arguments);
   }
 
   private void checkNumberOperands(Token operator, Object left, Object right) {
