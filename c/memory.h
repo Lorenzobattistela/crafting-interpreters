555251e230e7c3f3655f903a0159b3df0fe2d471
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-01-22 11:45:30 -0300

    creating chunks and memory management

---
+++ c/memory.h
@@ -0,0 +1,18 @@
+#ifndef clox_memory_h
+#define clox_memory_h
+
+#include "common.h"
+
+#define GROW_CAPACITY(capacity) ((capacity) < 8 ? 8 : (capacity) * 2)
+
+#define GROW_ARRAY(type, pointer, oldCount, newCount) (type*)reallocate(pointer, sizeof(type) * (oldCount), sizeof(type) * (newCount))
+
+#define FREE_ARRAY(type, pointer, oldCount) reallocate(pointer, sizeof(type) * (oldCount), 0)
+
+// oldSize: 0 newSize: Non-zero -> Op: Allocate new block
+// oldSize: Non-zero newSize: 0 -> Op: Free allocation
+// oldSize: Non-zero newSize: Smaller than oldSize -> Op: shrink existing allocation
+// oldSize: Non-zero newSize: Greather than oldSize -> Op> grow existing allocation
+void* reallocate(void* pointer, size_t oldSize, size_t newSize);
+
+#endif
