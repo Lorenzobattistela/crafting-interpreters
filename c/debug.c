7671ba37e03f8a298b3dea9ac913d52bb9ea893f
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-02-16 07:23:13 -0300

    adding cases for local variables get and set

--- c/debug.c
+++ c/debug.c
@@ -23,6 +23,12 @@ static int constantInstruction(const char* name, Chunk* chunk, int offset) {
   return offset + 2; // two byes: 1 for opcode and one for operand
 }
 
+static int byteInstruction(const char* name, Chunk* chunk, int offset) {
+  uint8_t slot = chunk->code[offset + 1];
+  printf("%-16s %4d\n", name, slot);
+  return offset + 2;
+}
+
 int disassembleInstruction(Chunk* chunk, int offset) {
   printf("%04d ", offset);
 
@@ -72,6 +78,10 @@ int disassembleInstruction(Chunk* chunk, int offset) {
       return constantInstruction("OP_GET_GLOBAL", chunk, offset);
     case OP_SET_GLOBAL:
       return constantInstruction("OP_SET_GLOBAL", chunk, offset);
+    case OP_GET_LOCAL:
+      return byteInstruction("OP_GET_LOCAL", chunk, offset);
+    case OP_SET_LOCAL:
+      return byteInstruction("OP_SET_LOCAL", chunk, offset);
     default:
       printf("Unkown opcode %d\n", instruction);
       return offset + 1;
