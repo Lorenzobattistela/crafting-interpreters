870856ad9c67a6932957f96a4e5d975cf03d1185
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-02-25 07:41:34 -0300

    [feat] starting to do functions

--- c/object.h
+++ c/object.h
@@ -8,19 +8,29 @@
 
 #define OBJ_TYPE(value) (AS_OBJ(value)->type)
 
+#define IS_FUNCTION(value) isObjType(value, OBJ_FUNCTION)
 #define IS_STRING(value) isObjType(value, OBJ_STRING)
 
+#define AS_FUNCTION(value) ((ObjFunction*)AS_OBJ(value))
 #define AS_STRING(value) ((ObjString*)AS_OBJ(value))
 #define AS_CSTRING(value) (((ObjString*)AS_OBJ(value))->chars)
 
 typedef enum {
   OBJ_STRING,
+  OBJ_FUNCTION,
 } ObjType;
 
 struct Obj {
   ObjType type;
   struct Obj* next;
 };
+
+typedef struct {
+  Obj obj;
+  int arity;
+  Chunk chunk;
+  ObjString* name;
+} ObjFunction;
 
 typedef struct ObjString {
   Obj obj;
@@ -29,6 +39,7 @@ typedef struct ObjString {
   uint32_t hash;
 } ObjString;
 
+ObjFunction* newFunction();
 ObjString* takeString(char* chars, int length);
 ObjString* copyString(const char* chars, int length);
 void printObject(Value value);
